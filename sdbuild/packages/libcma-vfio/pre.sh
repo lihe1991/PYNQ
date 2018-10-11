f118da55dd12bc23e0c8761dd42d691fe386c381
Peter Ogden <ogden@xilinx.com>
2018-10-11 06:49:42 -0700

    Allow non-root processes to allocate up to 1G of locked memory

--- sdbuild/packages/libcma-vfio/pre.sh
+++ sdbuild/packages/libcma-vfio/pre.sh
@@ -5,3 +5,4 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $script_dir/libxlnk_cma.h $target/usr/include
 sudo cp -r $script_dir/vfiocma.cpp $target/root
+sudo cp $script_dir/memlock.conf $target/etc/security/limits.d/
